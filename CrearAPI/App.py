c72209cef4f3fbf203b6e187c6dad7ae70307622
PRO X <bredalisgautreaux@gmail>
2024-08-29 07:17:40 +0200

    feat: ✨ Clases de APIs Rest

---
+++ CrearAPI/App.py
@@ -0,0 +1,17 @@
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+# Crear la primera API
+
+class HolaMundo(Resource):
+	def get(self):
+		return {"Hola": "Mundo"}
+
+api.add_resource(HolaMundo, "/")
+
+if __name__ == "__main__":
+	app.run(debug = True)
